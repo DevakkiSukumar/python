be989f4f38cac91ee0691e502111147e674cfebb
DevakkiSukumar <78472825+DevakkiSukumar@users.noreply.github.com>
2021-05-03 19:43:12 +0530

    Operator

---
+++ Operator.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the A value"))
+b=int(input("Enter the B value"))
+c=a=b
+d=a-b
+e=a*b
+f=a/b
+
+print("The add of two values",c)
+print("The sub of two values",d)
+print("The mul of two values",e)
+print("The div of two values",f)
