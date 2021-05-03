95cec703096125fb6b2a1eefa4be36123d4b81d0
DevakkiSukumar <78472825+DevakkiSukumar@users.noreply.github.com>
2021-05-03 19:41:21 +0530

    interger

---
+++ interger.py
@@ -0,0 +1,20 @@
+print ("\nFirst Method")
+a=b=c=10
+print (a) 
+print (b)
+print (c)
+
+print ("\nSecond Method")
+a,b,c="Devakki","Mca","3yr"
+print (a)
+print (b)
+print (c)
+
+print ("Third Method String")
+str ='Good Morning'
+print (str)
+print (str[5])
+print (str [0:4])
+print (str[4:0])
+print (str * 3)
+print (str + " Have a nice day")
