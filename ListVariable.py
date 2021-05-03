cd893ede9a451c1c6e5311be2af33da9e16eb957
DevakkiSukumar <78472825+DevakkiSukumar@users.noreply.github.com>
2021-05-03 19:42:29 +0530

    List Variable

---
+++ ListVariable.py
@@ -0,0 +1,11 @@
+print ("\nList Variable\n")
+a=['Devakki','mca',123]
+b=['abc',101,102,103]
+print (a)
+print (a[0])
+print ("\nUpdate the values\n")
+a[0]='pavi'
+print (a[0:])
+
+print ("\n",b[0:])
+print (b[1:4])
