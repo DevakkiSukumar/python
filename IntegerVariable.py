c2f3001226708be2ce188bea08b86e3b98ac489d
DevakkiSukumar <78472825+DevakkiSukumar@users.noreply.github.com>
2021-05-03 19:37:13 +0530

    IntegerVariable

---
+++ IntegerVariable.py
@@ -0,0 +1,13 @@
+print ("\nInteger Variable")
+print ("\nFirst Method")
+a=b=c=10
+print (a) 
+print (b)
+print (c)
+
+print ("\nSecond Method")
+a,b,c='Devakki','Mca','3yr'
+print (a)
+print (b)
+print (c)
+
